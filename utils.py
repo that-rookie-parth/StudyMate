dc34f1da9b259d4fb43e8e9441fdf3012c394c96
that-rookie-parth <kulshreshthaparth@gmail.com>
2023-07-13 00:44:33 +0530

    prevent multiple function call

---
+++ utils.py
@@ -0,0 +1,9 @@
+from indexing import index
+
+# function to provide the gpt with context of the given docs
+def get_similiar_docs(query,k=1,score=False):
+  if score:
+    similar_docs = index.similarity_search_with_score(query,k=k)
+  else:
+    similar_docs = index.similarity_search(query,k=k)
+  return similar_docs
