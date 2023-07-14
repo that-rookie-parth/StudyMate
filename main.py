0f53d1cee220e62df8afaa5b0ea6cebd68aea378
that-rookie-parth <kulshreshthaparth@gmail.com>
2023-07-15 00:45:20 +0530

    adding utility functions

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import streamlit as st
 from streamlit_extras.add_vertical_space import add_vertical_space
-from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain.memory import ConversationSummaryBufferMemory
+from langchain.chains import ConversationChain
+from langchain.chat_models import ChatOpenAI
+import os
+from dotenv import load_dotenv
+from utils import get_similiar_docs, query_refiner
+
+load_dotenv()
+llm = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=os.getenv("OPENAI_API_KEY"))
 
 st.set_page_config(page_title="StudyMate - Your Personalised AI Tutor")
 
@@ -19,22 +27,48 @@ with st.sidebar:
     """
     )
     add_vertical_space(2)
-    st.write("Made with ❤️ by [Rookie-Parth](https://github.com/that-rookie-parth/Chatbot)")
+    st.write(
+        "Made with ❤️ by [Rookie-Parth](https://github.com/that-rookie-parth/Chatbot)"
+    )
 
 st.header("StudyMate")
 
 # bot's response
 if "generated" not in st.session_state:
-    st.session_state['generated'] = ["Hi!, how can i assist you?"]
+    st.session_state["generated"] = ["Hi!, how can i assist you?"]
 
 # user input
 if "requests" not in st.session_state:
     st.session_state["requests"] = []
 
+# memory for the bot
+if "buffer_memory" not in st.session_state:
+    st.session_state["buffer_memory"] = ConversationSummaryBufferMemory(
+        llm=llm, max_token_limit=70, return_messages=True
+    )
+
+# containers for the app
 input_container = st.container()
 response_container = st.container()
 
-if "buffer_memory" not in st.session_state:
-    st.session_state.buffer_memory = ConversationBufferWindowMemory(
-        k=3, return_messages=True
-    )
+# conversation buffer window memory
+conversation = ConversationChain(
+    memory=st.session_state["buffer_memory"],
+    llm=llm,
+    verbose=True,
+)
+
+with input_container:
+    query = st.text_input("Query", key="input")
+    if query:
+        with st.spinner("typing...."):
+            refined_query = query_refiner(
+                st.session_state.buffer_memory.load_memory_variable({}), query
+            )
+            st.subheader("Refined Query:")
+            st.write(refined_query)
+            context = get_similiar_docs(refined_query)
+
+            response = conversation.predict(
+                input=f"Context:\n {context} \n\n Query:\n{refined_query}"
+            )
--- utils.py
+++ utils.py
@@ -1,12 +1,23 @@
 from indexing import index
+import openai
 
 # function to provide the gpt with context of the given docs
-def get_similiar_docs(query,k=1,score=False):
-  if score:
-    similar_docs = index.similarity_search_with_score(query,k=k)
-  else:
-    similar_docs = index.similarity_search(query,k=k)
-  return similar_docs
+def get_similiar_docs(query, k=1, score=False):
+    if score:
+        similar_docs = index.similarity_search_with_score(query, k=k)
+    else:
+        similar_docs = index.similarity_search(query, k=k)
+    return similar_docs
 
-def get_conversation_string():
-  conversation_string = ""
+# query refiner func
+def query_refiner(conversation, query):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
+    return response["choices"][0]["text"]
