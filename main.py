6fdf471066c97355b3211fb9b9f41232debd888b
that-rookie-parth <kulshreshthaparth@gmail.com>
2023-07-15 00:58:02 +0530

    added the prompt template

--- main.py
+++ main.py
@@ -5,7 +5,17 @@ from langchain.chains import ConversationChain
 from langchain.chat_models import ChatOpenAI
 import os
 from dotenv import load_dotenv
+from langchain import PromptTemplate, LLMChain
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
 from utils import get_similiar_docs, query_refiner
+from streamlit_chat import message
+
 
 load_dotenv()
 llm = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=os.getenv("OPENAI_API_KEY"))
@@ -58,6 +68,48 @@ conversation = ConversationChain(
     verbose=True,
 )
 
+# prompt template
+template = """As an esteemed expert and experienced teacher, your dedication to fostering students' learning is unparalleled. 
+Your role is to guide them with utmost care, answering their questions and preparing high-quality practice 
+materials. You understand the importance of thorough and descriptive explanations, ensuring that students grasp 
+concepts fully. Before formulating your response, you diligently search for relevant information in the 
+similar_docs to provide accurate and well-informed answers. Leveraging the power of GPT, you enhance your 
+responses by incorporating the model's expansive knowledge base and linguistic capabilities. 
+Additionally, you share the best sources and formulas, empowering students with reliable references 
+and tools to deepen their understanding. Your commitment to providing an exceptional learning experience 
+shines through in your meticulous preparation and comprehensive guidance, fostering a genuine love for 
+learning in each student you engage with."""
+system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+
+example_human = HumanMessagePromptTemplate.from_template(
+    "Describe the process of neutralization with the help of an example."
+)
+example_ai = AIMessagePromptTemplate.from_template(
+    """The reaction between an acid and a base is known as neutralization reaction. In this 
+    reaction, both acid and base cancel each others effect. Neutralisation reaction results 
+    in the formation of salt and water. During this reaction, energy in the form of heat is 
+    evolved.
+    
+    Acid + Base → Salt + Water + Heat
+    
+    For example, when sodium hydroxide (NaOH) is added to hydrochloric acid (HCl), 
+    sodium chloride (NaCl) and water (H2O) are obtained.
+    
+    HCl+NaOH⟶NaCl+H2O"""
+)
+
+human_template = "{input}"
+human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+chat_prompt = ChatPromptTemplate.from_messages(
+    [
+        system_message_prompt,
+        example_human,
+        example_ai,
+        human_message_prompt,
+    ]
+)
+
 with input_container:
     query = st.text_input("Query", key="input")
     if query:
@@ -72,3 +124,12 @@ with input_container:
             response = conversation.predict(
                 input=f"Context:\n {context} \n\n Query:\n{refined_query}"
             )
+
+with response_container:
+    if st.session_state["responses"]:
+        for i in range(len(st.session_state["responses"])):
+            message(st.session_state["responses"][i], key=str(i))
+            if i < len(st.session_state["requests"]):
+                message(
+                    st.session_state["requests"][i], is_user=True, key=str(i) + "_user"
+                )
