241646e36f2dad13d378f8b42fa1a5670e168fb7
that-rookie-parth <kulshreshthaparth@gmail.com>
2024-05-31 00:37:44 +0530

    made some changes due to deprecation of functions

--- indexing.py
+++ indexing.py
@@ -1,35 +1,33 @@
-from langchain.document_loaders import PyPDFDirectoryLoader
+from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import SentenceTransformerEmbeddings
+from langchain_community.embeddings import SentenceTransformerEmbeddings
 import pinecone
-from langchain.vectorstores import Pinecone
+from langchain_community.vectorstores import Pinecone
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
-PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
-PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT")
 
 # change the path accordingly
-directory = "D:\Chatbot\Content"
+directory = "Content"
 loader = PyPDFDirectoryLoader(directory)
 documents = loader.load()
-# print('Loading of doc done!')
-
+print('Loading of doc done!')
 
 def split_docs(documents, chunk_size=1000, chunk_overlap=150):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=chunk_size, chunk_overlap=chunk_overlap
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap
     )
     docs = text_splitter.split_documents(documents)
     return docs
 
+print("Total Splits: ",len(documents))
 docs = split_docs(documents)
-# print(len(docs))
+print("Total Splits: ",len(docs))
 
-embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+# embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 
-pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
-index_name = "chatbot"
-docsearch = Pinecone.from_documents(docs, embeddings, index_name=index_name)
-index = Pinecone.from_existing_index(index_name=index_name, embedding=embeddings)
+# pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
+# index_name = "chatbot"
+# docsearch = Pinecone.from_documents(docs, embeddings, index_name=index_name)
+# index = Pinecone.from_existing_index(index_name=index_name, embedding=embeddings)
