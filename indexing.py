8a92478f644ae7aac68e8492ac084b336993ed14
that-rookie-parth <kulshreshthaparth@gmail.com>
2024-06-07 12:19:13 +0530

    updated the code for uploading the data

--- indexing.py
+++ indexing.py
@@ -1,15 +1,17 @@
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import SentenceTransformerEmbeddings
-import pinecone
-from langchain_community.vectorstores import Pinecone
 from dotenv import load_dotenv
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+from pinecone import Pinecone, ServerlessSpec
+import os
+import time
+from langchain_pinecone import PineconeVectorStore
 
 load_dotenv()
 
 # change the path accordingly
 directory = "Content"
-loader = PyPDFDirectoryLoader(directory)
+loader = PyPDFDirectoryLoader(directory)    
 documents = loader.load()
 print('Loading of doc done!')
 
@@ -25,6 +27,42 @@ print("Total Splits: ",len(documents))
 docs = split_docs(documents)
 print("Total Splits: ",len(docs))
 
+model_name = "BAAI/bge-small-en"
+model_kwargs = {"device": "cuda"}
+encode_kwargs = {"normalize_embeddings": True}
+hf = HuggingFaceBgeEmbeddings(
+    model_name=model_name,
+    model_kwargs=model_kwargs,
+    encode_kwargs=encode_kwargs
+)
+model_dimensions = 384
+
+pc = Pinecone()
+p_index = os.getenv('pinecone_index_name')
+if p_index in pc.list_indexes().names():
+    print("Deleted the Existing Index")
+    pc.delete_index(p_index)
+
+print("Creating the index")
+pc.create_index(
+    name=p_index,
+    dimension=model_dimensions,
+    metric="cosine",
+    spec=ServerlessSpec(
+        cloud="aws",
+        region="us-east-1"
+    )
+)
+
+namespace = "studymate_chatbot"
+docsearch = PineconeVectorStore.from_documents(
+    documents=docs,
+    index_name=p_index,
+    embedding=hf, 
+    namespace=namespace 
+)
+
+time.sleep(1)
 # embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 
 # pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
